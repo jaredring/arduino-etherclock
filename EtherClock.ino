8f24fb727bc00608338db42b9cbe4e2bd61a4883
Jared Ring <jared@ring.gs>
2017-02-19 21:28:45 +1000

    More work on the NTP packcets

--- EtherClock.ino
+++ EtherClock.ino
@@ -18,9 +18,20 @@
 
 // Third Party Libs
 #include <Ethernet2.h>          // https://github.com/adafruit/Ethernet2
+#include <EthernetUdp2.h>       // https://github.com/adafruit/Ethernet2
 #include <LiquidTWI2.h>         // https://github.com/lincomatic/LiquidTWI2
 #include <DS3232RTC.h>          // https://github.com/JChristensen/DS3232RTC
 #include <Time.h>               // https://github.com/PaulStoffregen/Time
+
+// NTP Details
+unsigned int secsSinceLastNtpReq = 32000;
+const unsigned int secsBetweenNtpReqs = 10;   // Four Hours
+//const unsigned int secsBetweenNtpReqs = 21600;   // Four Hours
+unsigned int ntpLocalPort = 8123;
+char ntpServer[] = "tranquil.home.thanhandjared.net";
+const int NTP_PACKET_SIZE = 48;
+byte ntpPacketBuffer[NTP_PACKET_SIZE];
+EthernetUDP ntpUdp;
 
 // Setup the Pins
 // Reserved Pins:
@@ -45,6 +56,7 @@ byte mac[] = {
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
 };
 
+// Do we show the date or the temp
 bool show_date = true;
 
 void LCD_clearLine(int line) {
@@ -54,6 +66,7 @@ void LCD_clearLine(int line) {
     }
     lcd.setCursor(0, line);
 }
+
 
 void setup() {
 
@@ -100,15 +113,20 @@ void setup() {
 
     while (Ethernet.begin(mac) != 1) {
         Serial.println("No response from DHCP. Sleeping."); 
+        delay(5000);
     }
     LCD_clearLine(1);
     lcd.print(Ethernet.localIP());
+    Serial.print("Got IP: ");
+    Serial.println(Ethernet.localIP());
     delay(5000);
-
     lcd.clear();
+
+    ntpUdp.begin(ntpLocalPort);
+
 }
 
-// Maintain our DHCP Lease
+// Maintainbe our DHCP Lease
 // Based on examples from the Ethernet2 Library
 void dhcp_maintain() {
     switch (Ethernet.maintain()) {
@@ -141,6 +159,71 @@ void dhcp_maintain() {
     }
 }
 
+void sendNtpRequest(char* ntphost) {
+
+    // Initialise the Packet
+    memset(ntpPacketBuffer, 0, NTP_PACKET_SIZE);
+
+    ntpPacketBuffer[0] = 0b11100011;    // LI, Version, Mode
+    ntpPacketBuffer[1] = 0;             // Stratum
+    ntpPacketBuffer[2] = 6;             // Polling Interval (Really? What is this measured in, is it realistic?)
+    ntpPacketBuffer[3] = 0xEC;          // Peer Clock Precision (WHAT IS IT)
+    // 8 bytes of zero
+    ntpPacketBuffer[12] = 49;           // I justs love random magic numbers!
+    ntpPacketBuffer[13] = 0x4E;
+    ntpPacketBuffer[14] = 49;
+    ntpPacketBuffer[15] = 52;
+
+//    ntpUdp.beginPacket(ntphost, 123);
+    IPAddress tranquil(192, 168, 0, 249);
+    ntpUdp.beginPacket(tranquil, 123);
+    ntpUdp.write(ntpPacketBuffer, NTP_PACKET_SIZE);
+    ntpUdp.endPacket();
+    Serial.println("Sent an NTP Request");
+}
+
+
+void processNtpResponse() {
+
+    ntpUdp.read(ntpPacketBuffer, NTP_PACKET_SIZE);
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(ntpPacketBuffer[40], ntpPacketBuffer[41]);
+    unsigned long lowWord = word(ntpPacketBuffer[42], ntpPacketBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+
+}
+
 void loop() {
     // A second has elapsed - Update Clock
     if ((millis() % 1000) == 0) {
@@ -170,6 +253,19 @@ void loop() {
             lcd.print(year());
             show_date = !show_date;
         }
+
+        // See if we have an NTP response
+        if (ntpUdp.parsePacket()) {
+			Serial.println("Something is waiting for us");
+            processNtpResponse();
+        }
+        secsSinceLastNtpReq++;
+    }
+
+    if (secsSinceLastNtpReq > secsBetweenNtpReqs) {
+        Serial.println("NTP Update Required");
+        sendNtpRequest(ntpServer);
+		secsSinceLastNtpReq = 0;
     }
 
     dhcp_maintain();
