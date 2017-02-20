84c7aebb07ce4acb8ac353235ff042a73a1f1674
dfrencham <dannypf@gmail.com>
2017-02-20 23:13:26 +1000

    clean up
    Created const and utilities files, did some general clean up

---
+++ utility.cpp
@@ -0,0 +1,37 @@
+/* utility.ino */
+
+#include "utility.h"
+#include <LiquidTWI2.h>
+#include <Ethernet2.h>          // https://github.com/adafruit/Ethernet2
+#include <EthernetUdp2.h>       // https://github.com/adafruit/Ethernet2
+
+void LCD_clearLine(LiquidTWI2 lcdref, int line) {
+    lcdref.setCursor(0, line);
+    for (int i=0;i<17;i++)
+        lcdref.print(" ");
+    lcdref.setCursor(0, line);
+}
+
+// Maintainbe our DHCP Lease
+// Based on examples from the Ethernet2 Library
+void dhcp_maintain() {
+    switch (Ethernet.maintain()) {
+        case DHCP_RENEW_FAIL:
+            Serial.println("DHCP Renew Failed");
+            // Turn off NTP Fetching
+            break;
+        case DHCP_RENEW_SUCCESS:
+            Serial.println("DHCP Renew Success");
+            break;
+        case DHCP_REBIND_FAIL:
+            // how is this diff to Renew fail?
+            Serial.println("DHCP Rebind Failed");
+            // Assume turn off NTP Fetching
+            break;
+        case DHCP_REBIND_SUCCESS:
+            Serial.println("DHCP Rebind Success");
+            break;
+        default:
+            break;
+    }
+}
